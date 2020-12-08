7161e78225235076914e050b62cb1a53d2afcf26
andrew <cairnsy17@hotmail.co.uk>
2020-12-08 23:33:51 +0000

    simple data structure experiment

--- src/app/home.js
+++ src/app/home.js
@@ -4,137 +4,120 @@ import LineChart from "./linechart/linechart";
 import SelectBox from "./linechart/selectbox";
 import SelectButton from "./linechart/selectbutton";
 
-const ChartData = {
-  Assets: [
-    {
-      Property: [
-        {
-          key: "Zoopla Value",
-          values: [
-            {
-              value: 150000,
-              date: "16/10/2020",
-              verified: false,
-            },
-            {
-              value: 180000,
-              date: "16/11/2020",
-              verified: false,
-            },
-            {
-              value: 130000,
-              date: "16/12/2020",
-              verified: false,
-            },
-          ],
-        },
-        {
-          key: "Survey Valuation",
-          values: [
-            {
-              value: 138000,
-              date: "16/10/2020",
-              verified: true,
-            },
-          ],
-        },
-        {
-          key: "Mortgage Advance",
-          values: [
-            {
-              value: 115000,
-              date: "16/10/2020",
-              verified: true,
-            },
-          ],
-        },
-      ],
-      Pensions: [
-        {
-          key: "Zurich",
-          values: [
-            {
-              value: 200000,
-              date: "17/12/2009",
-              verified: true,
-            },
-          ],
-        },
-      ],
-    },
-  ],
-  Debts: [
-    {
-      "Mortgage(s)": [
-        {
-          key: "Danske Bank",
-          values: [
-            {
-              value: 115000,
-              date: "03/03/2018",
-              verified: true,
-            },
-            {
-              value: 125000,
-              date: "03/03/2019",
-              verified: true,
-            },
-            {
-              value: 145000,
-              date: "03/03/2020",
-              verified: true,
-            },
-            {
-              value: 185000,
-              date: "03/03/2021",
-              verified: true,
-            },
-          ],
-        },
-      ],
-      "Credit Card(s)": [
-        {
-          key: "Barclays",
-          values: [
-            {
-              value: 2000,
-              date: "03/03/2018",
-              verified: true,
-            },
-          ],
-        },
-      ],
-      "Bank Loan(s)": [
-        {
-          key: "HSBC",
-          values: [
-            {
-              value: 10000,
-              date: "01/01/2020",
-              verified: true,
-            },
-          ],
-        },
-      ],
-    },
-  ],
-  Insurance: [
-    {
-      "Policy type??": [
-        {
-          key: "Aviva",
-          values: [
-            {
-              value: 500000,
-              date: "02/03/2015",
-              verified: true,
-            },
-          ],
-        },
-      ],
-    },
-  ],
-};
+const ChartData = [
+  {
+    Group: "Assets",
+    Type: "Property",
+    name: "Zoopla",
+    value: 160000,
+    date: "01/01/2020",
+    verified: false,
+  },
+  {
+    Group: "Assets",
+    Type: "Property",
+    name: "Zoopla",
+    value: 170000,
+    date: "01/02/2020",
+    verified: false,
+  },
+  {
+    Group: "Assets",
+    Type: "Property",
+    name: "Zoopla",
+    value: 180000,
+    date: "01/03/2020",
+    verified: false,
+  },
+  {
+    Group: "Assets",
+    Type: "Property",
+    name: "Survey 1",
+    value: 138000,
+    date: "02/02/2020",
+    verified: false,
+  },
+  {
+    Group: "Assets",
+    Type: "Property",
+    name: "Survey 2",
+    value: 142000,
+    date: "03/03/2020",
+    verified: false,
+  },
+  {
+    Group: "Assets",
+    Type: "Property",
+    name: "Mortgage Advance",
+    value: 112000,
+    date: "04/04/2020",
+    verified: true,
+  },
+  {
+    Group: "Assets",
+    Type: "Pensions",
+    name: "Zurich",
+    value: 200000,
+    date: "05/05/2020",
+    verified: true,
+  },
+  {
+    Group: "Debts",
+    Type: "Mortgage",
+    name: "Danske",
+    value: 182000,
+    date: "06/06/2020",
+    verified: false,
+  },
+  {
+    Group: "Debts",
+    Type: "Mortgage",
+    name: "Northern",
+    value: 212000,
+    date: "07/07/2020",
+    verified: true,
+  },
+  {
+    Group: "Debts",
+    Type: "Mortgage",
+    name: "AIB",
+    value: 410000,
+    date: "08/08/2020",
+    verified: true,
+  },
+  {
+    Group: "Debts",
+    Type: "Credit cards",
+    name: "Barcleys",
+    value: 821000,
+    date: "09/09/2020",
+    verified: false,
+  },
+  {
+    Group: "Debts",
+    Type: "Credit cards",
+    name: "HSBC",
+    value: 121000,
+    date: "10/10/2020",
+    verified: true,
+  },
+  {
+    Group: "Insurance",
+    Type: "Life",
+    name: "LV",
+    value: 182000,
+    date: "11/11/2020",
+    verified: false,
+  },
+  {
+    Group: "Insurance",
+    Type: "Home",
+    name: "Aviva",
+    value: 212000,
+    date: "12/12/2020",
+    verified: true,
+  },
+];
 
 function Home() {
   const [dataSelection, setChildData] = useState("");
@@ -143,7 +126,7 @@ function Home() {
   return (
     <>
       <Grid fluid>
-        <Row>
+        {/* <Row>
           <Col xs={12} sm={12} md={6} lg={6} className="u-pt-gi">
             <Row start="lg" center="xs">
               <SelectBox ChartData={ChartData} passChildData={setChildData} />
@@ -158,7 +141,7 @@ function Home() {
               />
             </Row>
           </Col>
-        </Row>
+        </Row> */}
         <Row>
           <Col xs={12} sm={12} md={12} lg={12}>
             <LineChart
